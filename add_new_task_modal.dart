2c352d96757e9760159b480e916e18de07720b0b
A R Shahrear <arshahrear30@gmail.com>
2026-01-07 04:21:17 +0600

    10.2

--- add_new_task_modal.dart
+++ add_new_task_modal.dart
@@ -1,51 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:shahrearapp/task.dart';
 
 
-class AddNewTaskModal extends StatelessWidget {
-  const AddNewTaskModal({super.key});
+class AddNewTaskModal extends StatefulWidget {
+  const AddNewTaskModal({
+    super.key,
+    required this.onAddTap,
+  });
+
+  final Function(Todo) onAddTap;
+
+  @override
+  State<AddNewTaskModal> createState() => _AddNewTaskModalState();
+}
+
+class _AddNewTaskModalState extends State<AddNewTaskModal> {
+  final TextEditingController todoTEController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(17.0),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Add new todo',
-                style: Theme.of(context).textTheme.titleLarge,
-              ), // Text
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(
-                    context,
-                  ); //eta deyar karon e cancel button kaz  koray functional vabey
-                },
-                icon: const Icon(Icons.close),
-              ), // IconButton
-            ],
-          ), // Row
-
-          const SizedBox(height: 26), //box er asey pashe 16 kore jayga rakbay
-
-          TextFormField(
-            maxLines: 4, //eta dilay input box ta 4 line er moto jayga paibo
-
-            decoration: InputDecoration(
-              hintText: 'Enter your todo here',
-              enabledBorder: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(),
-            ), // InputDecoration
-          ), // TextFormField
-          const SizedBox(height: 16),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(onPressed: () {}, child: const Text('Add')),
-          ),
-        ],
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Add new todo',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 24,//box er asey pashe 16 kore jayga rakbay
+            ),
+            TextFormField(
+              maxLines: 4,//eta dilay input box ta 4 line er moto jayga paibo
+              controller: todoTEController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your todo here',
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+              validator: (String? value) {
+                if (value?.isEmpty ?? true) {
+                  return 'Enter a value';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Todo todo = Todo(details: todoTEController.text.trim(),
+                        createdDateTime: DateTime.now(),
+                        updatedDateTime: DateTime.now(),
+                      );
+                      widget.onAddTap(todo);
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text('Add'),
+                ))
+          ],
+        ),
       ),
     );
   }
